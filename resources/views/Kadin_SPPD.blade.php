1ad62da769d48f63661ed5a3ecc741244a53c1b4
Adiesukses <bintangempatprakarsa@gmail.com>
2022-11-18 22:08:17 +0700

    l

--- resources/views/Kadin_SPPD.blade.php
+++ resources/views/Kadin_SPPD.blade.php
@@ -164,7 +164,11 @@
                       <div class="col">
                         <label>KODE MATA ANGGARAN</label>
                         <select name="anggaran" class="form-control select2 select2-hidden-accessible" data-select2-id="1" tabindex="-1" aria-hidden="true">
-                          <option selected="selected" data-select2-id="3">- Pilih -</option>
+                         @foreach ($dina as $nana)
+                            <option value="{{ $nana->id }}">- Pilih -</option>
+                            <option value="">{{ $nana->kode_angaran }}</option>
+                         @endforeach
+                          
                  
                     
                 
