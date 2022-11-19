5e6c917ed31c67adf3f8c2baa6197e34fcd4acda
Adiesukses <bintangempatprakarsa@gmail.com>
2022-11-19 16:15:58 +0700

    kk

--- resources/views/cc.blade.php
+++ resources/views/cc.blade.php
@@ -2,15 +2,7 @@
 @section('halaman')
 <div class="form-row">
   
-  <div class="form-group col-md-12">
-    <label>PPTK</label>
-    <select name="pegawa" class="form-control select2 " data-select2-id="1" tabindex="-1" aria-hidden="true">
-        <option value="">- Pilih -</option> 
-                @foreach ($dina as $nana)
-                  <option value="{{ $nana->pegawai }}">{{ $nana->kode_angaran }}</option>
-                @endforeach
-    </select>
-</div>  
+ 
     
       <label></label>
       <button type="submit" class="btn btn-primary float-right">KIRIM DATA</button>
