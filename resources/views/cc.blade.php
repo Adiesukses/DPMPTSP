d4294120ed680240e543003cc194c211845732a6
Adiesukses <bintangempatprakarsa@gmail.com>
2022-11-19 20:39:58 +0700

    mm

--- resources/views/cc.blade.php
+++ resources/views/cc.blade.php
@@ -1,15 +1,7 @@
 @extends('layout.home')
 @section('halaman')
 <div class="form-row">
-      <div class="form-group col-md-12">
-            <label>PPTK</label>
-            <select name="pptk" class="form-control select2 " data-select2-id="1" tabindex="-1" aria-hidden="true">
-                <option value="">- Pilih -</option> 
-                        @foreach ($nominatif as $pptk)
-                          <option value="{{ $pptk->nama }}">{{  $pptk->nama }}</option>
-                        @endforeach
-            </select>
-        </div> 
+
  
     
       <label></label>
