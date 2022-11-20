a85ccd3ef2717182c7fcfeae3d027b20942925b9
Adiesukses <bintangempatprakarsa@gmail.com>
2022-11-20 22:17:17 +0700

    pp

--- resources/views/cc.blade.php
+++ resources/views/cc.blade.php
@@ -1,42 +1,7 @@
 @extends('layout.home')
 @section('halaman')
-<div class="form-row">
 
-      <div class="form-group col-md-6">
-            <label>PPTK</label>
-            <select name="pptk" class="form-control select2 select2-hidden-accessible"
-                data-select2-id="1" tabindex="-1" aria-hidden="true">
-                <option value="" required>-Pilih PPTK-</option>
-
-                <option value="">kkk</option>
-
-
-            </select>
-        </div>
-        <div class="form-group col-md-12">
-            <label>KODE MATA ANGGARAN</label>
-            <select name="anggaran" class="form-control select2 " required>
-                <option value="">- Pilih -</option>
-                {{-- @foreach ($dina as $nana)
-              <option value="{{ $nana->angaran }}">{{ $nana->angaran }}</option>
-                @endforeach --}}
-            </select>
-        </div>
-        <div class="form-group col-md-12">
-            <label>DASAR PERJALANAN</label>
-            <textarea name="dasar" class="form-control" required></textarea>
-        </div>
-        <div class="form-group col-md-12">
-            <label>TUJUAN</label>
-            <textarea name="tujuan" class="form-control" required></textarea>
-        </div>
-        <div class="form-group col-md-12">
-            <label>MAKSUD</label>
-            <textarea name="maksud" class="form-control" required></textarea>
-        </div>
-    
-      <label></label>
-      <button type="submit" class="btn btn-primary float-right">KIRIM DATA</button>
-</div>
-
+<div class="col-sm-6">
+      <span class="irs irs--flat js-irs-3"><span class="irs"><span class="irs-line" tabindex="0"></span><span class="irs-min" style="visibility: hidden;">-50°</span><span class="irs-max" style="visibility: visible;">50°</span><span class="irs-from" style="visibility: hidden;">0</span><span class="irs-to" style="visibility: hidden;">0</span><span class="irs-single" style="left: 0.052367%;">-49°</span></span><span class="irs-grid"></span><span class="irs-bar irs-bar--single" style="left: 0px; width: 2.16282%;"></span><span class="irs-shadow shadow-single" style="display: none;"></span><span class="irs-handle single" style="left: 0.976269%;"><i></i><i></i><i></i></span></span><input id="range_6" type="text" name="range_6" value="" class="irs-hidden-input" tabindex="-1" readonly="">
+    </div>
 @endsection
