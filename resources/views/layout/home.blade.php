f999c67dee0f82e5a06c2d177045b707795bd3a8
Adiesukses <bintangempatprakarsa@gmail.com>
2022-11-21 21:10:14 +0700

    aa

--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -307,7 +307,7 @@
 <script src="dist/js/demo.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="dist/js/pages/dashboard2.js"></script>
-
+<script src="/js/script.js"></script>
 {{-- @include('sweetalert::alert') --}}
 
 
