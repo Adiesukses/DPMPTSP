caa68fd67662c4f5e3adb85aedaf5a3c76742e99
Adiesukses <bintangempatprakarsa@gmail.com>
2022-11-23 21:23:15 +0700

    xx

--- resources/views/cc.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout.home')
-@section('halaman')
-
-<div class="col-sm-6">
-      <span class="irs irs--flat js-irs-3"><span class="irs"><span class="irs-line" tabindex="0"></span><span class="irs-min" style="visibility: hidden;">-50°</span><span class="irs-max" style="visibility: visible;">50°</span><span class="irs-from" style="visibility: hidden;">0</span><span class="irs-to" style="visibility: hidden;">0</span><span class="irs-single" style="left: 0.052367%;">-49°</span></span><span class="irs-grid"></span><span class="irs-bar irs-bar--single" style="left: 0px; width: 2.16282%;"></span><span class="irs-shadow shadow-single" style="display: none;"></span><span class="irs-handle single" style="left: 0.976269%;"><i></i><i></i><i></i></span></span><input id="range_6" type="text" name="range_6" value="" class="irs-hidden-input" tabindex="-1" readonly="">
-    </div>
-@endsection
