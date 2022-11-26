95f0827cddc5f8a3b9ac82e826902b99342ac7cb
Adiesukses <bintangempatprakarsa@gmail.com>
2022-11-26 11:54:52 +0700

    bb

--- resources/views/agenda/agendaFormEdit.blade.php
+++ resources/views/agenda/agendaFormEdit.blade.php
@@ -3,25 +3,25 @@
  
         <div class="form-group col-md-12">
             <label>KEGIATAN</label>
-            <textarea name="kegiatan" class="form-control" required></textarea>
+            <textarea name="kegiatan" class="form-control" required> {{ $ikilodatane->kegiatan }}</textarea>
         </div>
 
         <div class="form-group col-md-12">
             <label>PERANGKAT DAERAH</label>
-            <input name="opd" type="text" class="form-control" required>
+            <input name="opd" type="text" class="form-control" value="{{ $ikilodatane->opd }}"required>
         </div>
 
     <div class="form-group col-md-12">
         <label>DEADLINE</label>
-        <input name="deadline" type="date" class="form-control" required>
+        <input name="deadline" type="date" class="form-control"value="{{ $ikilodatane->deadline }}" required>
     </div>
     <div class="form-group col-md-12">
         <label>PROGRESS</label>
-        <input name="progres" type="number" class="form-control" required>
+        <input name="progres" type="number" class="form-control"value="{{ $ikilodatane->progres }}" required>
     </div>
     <div class="form-group col-md-12">
         <label>KETERANGAN</label>
-        <textarea name="keterangan" class="form-control" required></textarea>
+        <textarea name="keterangan" class="form-control"required>{{ $ikilodatane->keterangan }}</textarea>
     </div>
 
     <div class="form-group col-md-12">
