7a637ddb5d3f08ed4557871f43bcdb86f819de92
Adiesukses <bintangempatprakarsa@gmail.com>
2022-11-27 00:38:08 +0700

    MM

--- routes/web.php
+++ routes/web.php
@@ -19,20 +19,20 @@ use App\Http\Controllers\controller_agenda;
 */
 Route::get('/',[controller_dashboard::class,'index']);
 Route::get('/Dashboard', [controller_dashboard::class,'index']);
-//----------------------------------------------------------------------
+//---------------------------SPPD-------------------------------------------
 Route::get('/kadinsppd', [controller_sppdkadin::class,'index']);
 Route::get('/kadinsppdctk/{id}', [controller_sppdkadin::class,'cetak']);
 Route::get('/kadinsppdctk2/{id}', [controller_sppdkadin::class,'cetak2']);
 Route::post('/tambahsppd', [controller_sppdkadin::class,'store']);
 Route::post('/tambahrek', [controller_rekening::class,'store']);
-//----------------------------------------------------------------------
+//--------------------------NOMINATIF--------------------------------------------
 Route::get('/nominatif', [controller_nominatif::class,'index']);
 Route::get('/nominatifList', [controller_nominatif::class,'listData']);
 Route::post('/nominatifTambah', [controller_nominatif::class,'store']);
 Route::post('/nominatifData', [controller_nominatif::class,'getData']);
 Route::post('/nomUpdate/{id}', [controller_nominatif::class,'update']);
 Route::get('/nomDelete{id}', [controller_nominatif::class,'destroy']);
-//----------------------------------------------------------------------
+//-------------------------AGENDA---------------------------------------------
 Route::get('/agenda', [controller_agenda::class,'index']);
 Route::get('/agendaList', [controller_agenda::class,'listData']);
 Route::post('/agendaTambah', [controller_agenda::class,'store']);
