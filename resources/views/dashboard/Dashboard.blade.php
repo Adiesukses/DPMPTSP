05a22e0e6c5421e991e240b293d662095f052e20
Adiesukses <bintangempatprakarsa@gmail.com>
2022-12-05 17:25:02 +0700

    cc

--- resources/views/dashboard/Dashboard.blade.php
+++ resources/views/dashboard/Dashboard.blade.php
@@ -280,8 +280,8 @@
                         <div class="form-group col-md-12">
                             <label>DISPOSISI</label>
                             <select name="disposisi" class="form-control select2 select2-hidden-accessible"
-                                style="width: 100%;" data-select2-id="1" tabindex="-1" aria-hidden="true">
-                                <option >--Disposisi--</option>
+                                style="width: 100%;" data-select2-id="1" tabindex="-1" aria-hidden="true" placeholder="Pilih" required>
+
                                 <option>Kasubbag Umum</option>
                                 <option>Kasubbag Umum & Staff Perencanaan</option>
                                 <option>Kasubbag Umum & Staff Kepegawaian </option>
