62224f433010d36c14c228c9fbe0cdfe470f2c80
Adiesukses <bintangempatprakarsa@gmail.com>
2022-12-05 17:21:09 +0700

    dd

--- resources/views/dashboard/Dashboard.blade.php
+++ resources/views/dashboard/Dashboard.blade.php
@@ -281,7 +281,7 @@
                             <label>DISPOSISI</label>
                             <select name="disposisi" class="form-control select2 select2-hidden-accessible"
                                 style="width: 100%;" data-select2-id="1" tabindex="-1" aria-hidden="true">
-                                <option selected="selected" data-select2-id="3">--Disposisi--</option>
+                                <option>--Disposisi--</option>
                                 <option>Kasubbag Umum</option>
                                 <option>Kasubbag Umum & Staff Perencanaan</option>
                                 <option>Kasubbag Umum & Staff Kepegawaian </option>
